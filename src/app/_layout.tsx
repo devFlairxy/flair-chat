e9359911345131acefedc7178fc90b26bd31eae1
Poseidon <122105735+poseidonxy@users.noreply.github.com>
2024-07-18 13:06:38 +0100

    install stream chat

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,9 +1,14 @@
 import { View, Text } from 'react-native';
 import React from 'react';
 import { Slot, Stack } from 'expo-router';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 const AppLayout = () => {
-  return <Slot />;
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Slot />
+    </GestureHandlerRootView>
+  );
 };
 
 export default AppLayout;
