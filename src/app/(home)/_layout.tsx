0636ae2d433c5093ac69495988b6d968b50e30d5
Poseidon <122105735+poseidonxy@users.noreply.github.com>
2024-07-18 14:55:23 +0100

    stream chat provider

--- src/app/(home)/_layout.tsx
+++ src/app/(home)/_layout.tsx
@@ -1,40 +1,17 @@
-import { View, Text } from 'react-native';
-import React, { useEffect } from 'react';
-import { Slot, Stack } from 'expo-router';
-import { StreamChat } from 'stream-chat';
-import { Chat, OverlayProvider } from 'stream-chat-expo';
-const client = StreamChat.getInstance('4fdycwe4d4gp');
+import { Stack } from 'expo-router';
+import ChatProvider from '../providers/ChatProvider';
 
 const HomeLayout = () => {
-  useEffect(() => {
-    const connect = async () => {
-      await client.connectUser(
-        {
-          id: 'jlahey',
-          name: 'Jim Lahey',
-          image: 'https://i.imgur.com/fR9Jz14.png',
-        },
-        client.devToken('jlahey')
-      );
-      // const channel = client.channel('messaging', 'the_park', {
-      //   name: 'The Park',
-      // });
-      // await channel.watch();
-    };
-    connect();
-  }, []);
   return (
-    <OverlayProvider>
-      <Chat client={client}>
-        <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen
-            name="channel/[cid]"
-            options={{ headerTitle: 'Channel', headerBackTitle: 'Chats' }}
-          />
-        </Stack>
-      </Chat>
-    </OverlayProvider>
+    <ChatProvider>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen
+          name="channel/[cid]"
+          options={{ headerTitle: 'Channel', headerBackTitle: 'Chats' }}
+        />
+      </Stack>
+    </ChatProvider>
   );
 };
 
